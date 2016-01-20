ba915fe0810731c677fa7cac44175d47b07098ed
Tim Murphy <tim@26tp.com>
2016-01-20 16:37:30 +1100

    Add solution and projects
    - Add git configuration
    - Add solution
    - Add project
    - Add test project
    - Update README.md

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 This repository, [croquet-australia-domain](https://github.com/croquet-australia/croquet-australia-domain), contains the main logic for all Croquet Australia resources. The `croquet-australia-domain` repository depends on **zero** Croquet Australia repositories. 
 
 This branch, `phase2/master`, is the current development branch.
+
+## todo
+
+- Add NuGet packages:
+    - Anotar.???.Fody
+    - EmptyStringGuard.Fody
+    - NullGuard.Fody
---
+++ tests/CroquetAustralia.Domain.Specifications/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CroquetAustralia.Domain.Specifications")]
+[assembly: AssemblyDescription("Specifications for CroquetAustralia.Domain project.")]
+[assembly: AssemblyCompany("Croquet Australia")]
+[assembly: AssemblyProduct("Croquet Australia")]
+[assembly: AssemblyCopyright("Copyright © 2016 Tim Murphy")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
