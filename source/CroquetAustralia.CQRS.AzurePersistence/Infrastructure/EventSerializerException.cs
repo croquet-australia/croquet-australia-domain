509e6ab282d71e7b3b9b1145d9804c9f30f1d413
Tim Murphy <tim@26tp.com>
2016-02-09 04:18:26 +1100

    Improve EventSerializer's exception reporting

---
+++ source/CroquetAustralia.CQRS.AzurePersistence/Infrastructure/EventSerializerException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CroquetAustralia.CQRS.AzurePersistence.Infrastructure
+{
+    public class EventSerializerException : Exception
+    {
+        public EventSerializerException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
